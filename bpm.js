fe9b9b27efb1a980b1ef9a7ebdd2af9e0a1e8ffa
Mary Lynn Tom <vivicca28@gmail.com>
2018-03-04 01:59:32 -0800

    Add files via upload

---
+++ bpm.js
@@ -0,0 +1,36 @@
+function calculateBpm() {
+        var bpm = prompt("What was your calculated number? (Enter a number 28 to 100.)");
+        bpm = bpm * 2;
+        console.log(bpm);
+
+        // 60 BPM
+        if (bpm >= 55 && bpm < 65) {
+            window.open("https://open.spotify.com/user/fippel/playlist/0oQegc01ommppaK2U5Jc1Y", "_self")
+        } else if (bpm >= 65 && bpm < 75) {
+            window.open("https://open.spotify.com/user/ever13more/playlist/2jtvGahlwnygZGEu6JYzLd", "_self");
+        } else if (bpm >= 75 && bpm < 85) {
+            window.open("https://open.spotify.com/user/andresrueda/playlist/42AEKVDXN3RKJHSS0m5c9c", "_self");
+        } else if (bpm >= 85 && bpm < 95) {
+            window.open("https://open.spotify.com/user/andresrueda/playlist/2rI1c77qoulP0to3jHlV8A", "_self");
+        } else if (bpm >= 95 && bpm < 105) {
+            window.open("https://open.spotify.com/user/americanheartassociation/playlist/6larteGNSuXdBhaYKLybwa", "_self");
+        } else if (bpm >= 105 && bpm < 115) {
+            window.open("https://open.spotify.com/user/andresrueda/playlist/33xdlgwdcshazSpmOp5fGm", "_self");
+        } else if (bpm >= 115 && bpm < 125) {
+            window.open("https://open.spotify.com/user/andresrueda/playlist/3RwxvR6nVkCm6WnJsCI2UO", "_self");
+        } else if (bpm >= 125 && bpm < 135) {
+            window.open("https://open.spotify.com/user/andresrueda/playlist/3QU0mV4lxSF3vogE3dqwFZ", "_self");
+        } else if (bpm >= 135 && bpm < 145) {
+            window.open("https://open.spotify.com/user/andresrueda/playlist/0V3DXv1ZQceXcoRqjY04rP", "_self");
+        } else if (bpm >= 145 && bpm < 155) {
+            window.open("https://open.spotify.com/user/andresrueda/playlist/3uoOmOFRXtFawneian2FnQ", "_self");
+        } else if (bpm >= 155 && bpm < 165) {
+            window.open("https://open.spotify.com/user/henkjanvanderklis/playlist/1n7bgisLYLfOwitTSwMBQE", "_self");
+        } else if (bpm >= 165 && bpm < 175) {
+            window.open("https://open.spotify.com/user/dauran/playlist/03oyYjZRxCRxMKzXJ3Xy6J", "_self");
+        } else if (bpm >= 175 && bpm < 187) {
+            window.open("https://open.spotify.com/user/alshober/playlist/65iCx1RzcjUHKO90nl0FgT", "_self");
+        }else if (bpm >= 187 && bpm < 200) {
+            window.open("https://open.spotify.com/user/alshober/playlist/65iCx1RzcjUHKO90nl0FgT", "_self");
+        }
+};
